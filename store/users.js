76aac812543ae6653db68040e95bf5b844854bfb
Leonard <leonard@Leonards-MacBook-Air.local>
2020-08-29 02:02:45 +0300

    Initial commit

---
+++ store/users.js
@@ -0,0 +1,26 @@
+const users = [
+  {
+    id: 1,
+    name: "Leonard Busoi",
+    email: "leo@gmail.com",
+    password: "123456",
+  },
+];
+
+const getUsers = () => users;
+
+const getUserById = (id) => users.find((user) => user.id === id);
+
+const getUserByEmail = (email) => users.find((user) => user.email === email);
+
+const addUser = (user) => {
+  user.id = users.length + 1;
+  users.push(user);
+};
+
+module.exports = {
+  getUsers,
+  getUserByEmail,
+  getUserById,
+  addUser,
+};
